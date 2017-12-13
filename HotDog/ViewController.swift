a335de9bd6ae2a2697f954625b0f6f45e398b074
Greg Weiss <gergusa04@gmail.com>
2017-12-13 09:29:32 -0500

    make a new dog

--- HotDog/ViewController.swift
+++ HotDog/ViewController.swift
@@ -12,12 +12,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let newdog = makeHotdog(ingredienrts: 3)
+        print(newdog)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func makeHotdog(ingredienrts: Int) -> Int {
+        var hotdog = 0
+        hotdog += ingredienrts
+        
+        return hotdog
     }
 
 
